aa0ee1627e68fab5d4d94c7f68befb5e17b9c679
Your Name <625230100@qq.com>
2016-06-22 17:41:02 +0800

    newPL

---
+++ Retrofit2/app/src/main/java/com/finddreams/retrofit/ui/textgit.java
@@ -0,0 +1,18 @@
+package com.finddreams.retrofit.ui;
+
+/**
+ * Created by 62523 on 2016/6/22.
+ */
+public class textgit {
+
+
+    private String asd;
+
+    public String getAsd() {
+        return asd;
+    }
+
+    public void setAsd(String asd) {
+        this.asd = asd;
+    }
+}
